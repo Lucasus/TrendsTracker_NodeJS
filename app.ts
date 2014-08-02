3afb9dff696a36043551932ad856c6065c545904
Łukasz Wiatrak <lukasz.wiatrak@gmail.com>
2014-08-02 10:20:23 +0200

    Express installed

--- app.ts
+++ app.ts
@@ -1,19 +1,20 @@
 ///<reference path="typings/references.ts" />
-var fs = require("fs");
-var express = require("express");
-var http = require("http");
+
+import fs = require("fs");
+import express = require("express");
+import http = require("http");
 
 var config = JSON.parse(fs.readFileSync("config.json", "utf-8"));
 
 var app = express();
 app.set("port", config.port);
-app.get("/", function (request, response) {
+app.get("/", function(request, response){
     response.send("hello!");
 });
 
 var server = http.createServer();
 server.on('request', app);
-server.listen(app.get("port"), function () {
+server.listen(app.get("port"), function() {
     console.log("Express server listening on port " + app.get("port"));
 });
-//# sourceMappingURL=app.js.map
+
